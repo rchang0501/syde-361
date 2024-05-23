52cc1cfca2c5e75c62d1c2622fe41d6d19403619
Ryan Chang <ryanchang0501@gmail.com>
2024-05-23 15:37:34 -0400

    add dict

--- knapsack.cpp
+++ knapsack.cpp
@@ -38,10 +38,10 @@ int main() {
     vector<pair<int, int>> pump_water = {{10, 0}};
     vector<pair<int, int>> change_dir_of_water = {{8, 5}, {6, 10}, {7, 15}, {6, 10}};
     vector<pair<int, int>> change_flow_rate = {{9, 10}, {7, 5}, {8, 12}, {8, 20}};
-    vector<pair<int, int>> spin_turbine = {{7, 20}, {9, 25}, {6, 15}, {8, 10}};
+    vector<pair<int, int>> spin_turbine = {{10, 15}};
     vector<pair<int, int>> discharge_water = {{8, 5}, {7, 15}, {6, 10}, {9, 5}};
     vector<pair<int, int>> motor = {{10, 0}};
-    vector<pair<int, int>> ac_to_dc = {{9, 5}, {7, 2}, {8, 10}, {10, 20}};
+    vector<pair<int, int>> ac_to_dc = {{10, 0}};
     vector<pair<int, int>> store_electricity = {{7, 15}, {9, 25}, {10, 25}, {8, 20}};
     vector<pair<int, int>> usb = {{10, 0}};
 
@@ -56,30 +56,36 @@ int main() {
     input.push_back(store_electricity);
     input.push_back(usb);
 
+    string dict[9][4] = {
+        {"water pump", "", "", ""},
+        {"PVC elbow joint", "Adjustable hose nozzle", "Flexible hose", "Rubber tubing with clamps"},
+        {"Ball valve", "Adjustable clamp", "Flow restrictor", "Inline water flow regulator"},
+        {"3D printed turbine", "", "", ""},
+        {"PVC pipe outlet", "Flexible discharge hose", "Drainage spout", "Gravity-fed return system"},
+        {"motor", "", "", ""},
+        {"full wave rectifier", "", "", ""},
+        {"Rechargeable AA batteries", "Supercapacitor", "Lithium-ion battery", "18650 battery"},
+        {"USB", "", "", ""}
+    };
+
     priority_queue<Solution, vector<Solution>, decltype(cmp)> pq(cmp);
 
     Solution curr = Solution(input.size());
     combinations(0, curr, pq, input);
 
-    for(int i = 0 ; i < 5 ; i++) {
+    for(int i = 0 ; i < 10 ; i++) {
         Solution curr = pq.top(); pq.pop();
         cout << curr.e << ", " << curr.c << ", [";
-        for(int j: curr.items) cout << j << ",";
-        cout << "]\n";
+        for(int r = 0 ; r < curr.items.size() ; r++) {
+            int c = curr.items[r];
+            cout << dict[r][c] << ", "; 
+        }
+        cout << "]\n\n";
     }
-
 
     return 0;
 }
 
 /*
-- water pump
-- pvc elbow
-- ball valve
-- hydro turbine generator
-- gravity fed
-- motor
-- dc adapter
-- lithium ion
-- usb
+86, 60, [water pump, PVC elbow joint, Ball valve, 3D printed turbine, Gravity-fed return system, motor, full wave rectifier, Lithium-ion battery, USB, ]
 */
